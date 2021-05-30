0057d6b4694b09b8381a70fc1bcd59a7b47463d8
BlackRabbit21 <81833689+BlackRabbit21@users.noreply.github.com>
2021-05-30 09:39:31 -0600

    Update Arduino_Code_bomba_de_agua.ino

--- 7-ArchivosdeCódigoFuente(Arduino)/Arduino_Code_bomba_de_agua.ino
+++ 7-ArchivosdeCódigoFuente(Arduino)/Arduino_Code_bomba_de_agua.ino
@@ -1,91 +1,103 @@
 
-//---------------------- VARIBABLES -------------------//
+//---------------------- VARIBABLES -------------------------------------------------------------//
 
-//---------------------- VARIBABLES DEL SENSOR DE NIVEL DE AGUA -------------------//
+//---------------------- VARIBABLES DEL MODULO DE RELAY ----------------------------------------//
+//PIN DEL RELE;
+int PINR1 = 2;
+//--------------------------------------------------------------------------------------------//
+
+//----------------------- VARIBABLES DEL SENSOR DE NIVEL DE AGUA ------------------------------//
+
 // VARIABLE DE EL VALOR QUE DE SENSOR DE AGUA;
 int SENSORWATER;
 //PIN DEL SENSOR DE AGUA;
 int  PINSENSORWATER = A0;
-//PIN DEL LED;
-int PINLED = 6;
-int PINMOTOR = 2;
-//----------------------------------------------------//
+//--------------------------------------------------------------------------------------------//
 
-//---------------------- VARIBABLES DEL SENSOR DE NIVEL DE DISTANCIA(ULTRASONAR) -------------------//
+//---------------------- VARIBABLES DEL SENSOR DE NIVEL DE DISTANCIA(ULTRASONAR) ------------//
 // VARIABLE PIN TRIGER DELULTRASONAR; 
 int PINTRIGER = 3;
 // VARIABLE PIN ECO DELULTRASONAR;
 int PINECO = 8;
 int DURACION;
 float DISTANCIA;
+//--------------------------------------------------------------------------------------------//
 
-//----------------------------------------------------//
+//--------------------------------------------------------------------------------------------//
 
-//----------------------------------------------------//
 
-void setup() 
-{
+void setup() {
+  // put your setup code here, to run once:
+ pinMode(PINR1, OUTPUT);
  pinMode(PINTRIGER, OUTPUT);
  pinMode(PINECO, INPUT);
- pinMode(PINLED, OUTPUT);
- pinMode(PINMOTOR, OUTPUT);
- Serial.begin(9600);
+  Serial.begin(9600);
 }
+
 void loop() {
-  
-  //APAGADO DE EL ULTRASONAR
+  // put your main code here, to run repeatedly:
+
+      //APAGADO DE EL ULTRASONAR
    digitalWrite(PINTRIGER, LOW);
   delayMicroseconds(2);
 
   //ENCENDIDO DE EL ULTRASONAR
   digitalWrite(PINTRIGER, HIGH);
    delayMicroseconds(10);
+
+       //APAGADO DE EL ULTRASONAR
    digitalWrite(PINTRIGER, LOW);
 
-  //SACAMOS LOS DATOS DEL PIN DE SENSOR DE AGUA
-   SENSORWATER = analogRead(PINSENSORWATER); 
+    //SACAMOS LOS DATOS DEL PIN DE SENSOR DE AGUA
+   SENSORWATER = analogRead(PINSENSORWATER);
 
-  //SACAMOS LOS DATOS DEL PIN DE EL ULTRASONAR
+ //SACAMOS LOS DATOS DEL PIN DE EL ULTRASONAR
   DURACION = pulseIn(PINECO,HIGH);  
 
-  //COMVERIMOS LOS DATOS RESIVIDOS EN DISTANCIA(CM)
+  //CALCULAMOS LA DISTANCIA EN CM
   DISTANCIA = ( DURACION*0.034)/2;
-   //DISTANCIA = DURACION / 58.2;
+  
+//------------------------------ CODIGO DE ENCENDIDO Y APAGADO DEL RELAY -------------------// 
+   //DECICION DE EL ENCENDIDO DEL RELAY
 
-   //IMPRIMIMOS LOS DATOS EN LA CONSOLA
-  Serial.print("//");
- Serial.print(SENSORWATER);
-  Serial.print("//");
-  Serial.print(DISTANCIA);
- Serial.print("CM");
+  // if(DISTANCIA <= 13.00 )
   if(DISTANCIA >= 13.00 )
   {
-                    Serial.print("A");
-                  digitalWrite(PINMOTOR,LOW);
-                 // digitalWrite(PINLED,HIGH);
-                delay(5000);             
- } 
+    //Serial.print(DISTANCIA);
+   // Serial.print("CM");
+    // Serial.print("/A/");
+     
+      //APAGADO DEL RELAY
+       digitalWrite(PINR1,HIGH);
+       delay(5000);  
+
+   
+  }
   else{
-    Serial.print("E");
-//if para encender el motor electrico cuando el agua este 35% de su capasidad
- if(SENSORWATER <= 350  )
-  {
-                  Serial.print("EN");
-                  digitalWrite(PINMOTOR,LOW);
-                delay(5000); 
-                  digitalWrite(PINMOTOR,HIGH);
-                delay(3000); 
-               
-               //Esperamos 1 segundos para mantener ensedido la bomba
-             
- } 
-  else{
-     Serial.print("AP");
-   //  Serial.print("//A");
-       
-        digitalWrite(PINMOTOR,LOW);
-         delay(5000); 
-     }    
-     } 
-      
+    //  Serial.print(DISTANCIA);
+   // Serial.print("CM");
+   //  Serial.print("/E/");
+    
+        //NIVEL DE AGUA EN EL 35 PORCIRNTO DE AGUA PAR ENCENDER EL RELAY
+    if(SENSORWATER <= 105){
+                 // Serial.print("/EN/");
+                  //Serial.print("//");
+                //Serial.print(SENSORWATER);
+                  //Serial.print("//");
+                  
+                  //ENCENDIDO DEL RELAY
+                 digitalWrite(PINR1,LOW);
+                  delay(3000); 
+    }
+    else{
+                //Serial.print("//");
+                  //Serial.print(SENSORWATER);
+                 // Serial.print("//");
+                 //Serial.print("/Ap/");
+
+                 //APAGADO DEL RELAY
+                   digitalWrite(PINR1,HIGH);
+                   delay(5000);    
+      }            
+    }   
 }
